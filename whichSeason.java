24aa3773d87271897277fa7a1c5676ca6fce499d
singhj22 <42975589+singhj22@users.noreply.github.com>
2018-11-13 12:45:46 -0500

    Add files via upload

---
+++ whichSeason.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Jugat Singh
+ * Which Season
+ */
+import java.util.Scanner;
+
+public class whichSeason 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner user= new Scanner(System.in);
+		
+		
+		
+	}
+
+}
